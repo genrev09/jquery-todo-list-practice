e54e331b6b91760084472ca7a8d6222599b0b1cf
Genrev Arambulo <arambge@oocl.com>
2019-10-02 09:19:58 +0800

    Committed todo.js. Added the following functionalities:
    - Add
    - Rename (Double Click, focusout event)
    - Cross out (Checkbox Checked)
    - Filter (All, Active, Completed)

--- js/todo.js
+++ js/todo.js
@@ -22,4 +22,69 @@ $(document)
         }
 
         // code to be implemented
+        //Button Click (Add)
+        $("#button").click(function(){
+            var tb_name = document.getElementById("tb_name").value;
+
+            if(tb_name){
+                $('ol').append('<li id="'+ generateUUID() + '" class=""> <input name="done-todo" type="checkbox" class="done-todo"> <span>' + document.getElementById("tb_name").value + ' </span> </li>');
+                document.getElementById("tb_name").value = "";
+                filterData();
+            }
+        }); 
+
+        //Select Filter
+        $("#filters").on('click','li', function(){
+            
+            //change the active button
+            document.getElementsByClassName("selected")[0].className = "";
+            $(this).children('a').addClass('selected');
+
+            //filter data
+            filterData();
+        });
+
+        //Double Click (Rename)
+        $("#ol_item").on('dblclick','span', function(){
+            this.setAttribute('contenteditable','true');
+            this.setAttribute('focus','true');
+        });
+
+        // Out of focus
+        $("#ol_item").on('focusout','span', function(){
+            this.setAttribute('focus','false');
+            this.setAttribute('contenteditable','false');
+        });
+
+        //Checkbox checked
+        $('body').on('click', '.done-todo', function(){
+            if (this.checked) 
+              $(this).closest("li").addClass("checked")
+             else 
+              $(this).closest("li").removeClass("checked")
+
+            filterData();
+        });
     });
+
+    function filterData(){
+        var temp_id = document.querySelector('.selected').id;
+
+        $('#ol_item li').each(function(){
+            if(temp_id == "all"){
+                this.hidden = false;
+            } else if (temp_id == "active") {
+                if(this.childNodes[1].checked){
+                    this.hidden = true;
+                } else {
+                    this.hidden = false;
+                }
+            } else if (temp_id == "complete") {
+                if(!this.childNodes[1].checked){
+                    this.hidden = true;
+                } else {
+                    this.hidden = false;
+                }
+            }
+        });
+    }
